032115c9c38dba1d579386fd0b01eaf9e765ed6a
ZRL75368 <zrl75368@educastur.es>
2026-09-25 13:57:47 +0200

    Actualizar

--- EJER_02/2.3.js
+++ EJER_02/2.3.js
@@ -8,5 +8,19 @@ const perfil = {
     empresa: 'IES Fleming'
 }
 
-const empleado = {...usuario, ...perfil}
-console.log(empleado);
+// Combinamos anidando el objeto perfil dentro de empleado
+const empleado = {
+    ...usuario,
+    perfil: {
+        ...perfil
+    }
+};
+
+console.log("Objeto empleado:", empleado);
+
+// Intentamos acceder a empleado.perfil.direccion.ciudad usando Optional Chaining
+// y asignamos un valor por defecto con Nullish Coalescing
+const ciudadEmpleado = empleado.perfil?.direccion?.ciudad ?? "Ciudad no especificada";
+
+console.log("Ciudad:", ciudadEmpleado); 
+// Resultado en consola: "Ciudad no especificada" (porque direccion no existe todavía)s
